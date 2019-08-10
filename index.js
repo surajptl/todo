b898ea704cbce13dcb160b411971dc0ab2963db1
surajptl <suraj326len@gmail.com>
2019-08-10 18:41:44 +0530

    Delete button work

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var arr = [];
 var i = 0;
-
+let temp = 0;
+function remove(x){
+    var elem = document.getElementById(x.target.id);
+    var liobj = elem.parentElement;
+    var ulObj = liobj.parentElement;
+    ulObj.removeChild(liobj);
+    // console.log(elem);
+   }  
+function changeText(y){
+  var elem = document.getElementById(y.target.id);
+  var liObj = elem.parentElement;
+  liObj.style.textDecoration = "line-through";
+}  
 function search(val) {
     var str = "";
     var ch = false;
@@ -9,16 +21,28 @@ function search(val) {
             alert("Please Enter Something!")
         } else {
             var obj = {};
+            
             obj[i++] = val.value;
             obj['check'] = ch;
             var ul = document.getElementsByClassName("append");
+              
             var bt1 = document.createElement("button");
             bt1.setAttribute("class","btn1 fas fa-check");
+            bt1.setAttribute("id","fid"+temp++);
+            bt1.addEventListener("click",changeText,false);
             var lb = document.createElement("label");
+
             var btn2 = document.createElement("button");
             btn2.setAttribute("class","btn2 fas fa-times");
+            // btn2.setAttribute("onclick",'deleteIt(id)');
+            btn2.setAttribute("id",'cid'+temp++);
+            btn2.addEventListener("click",remove,false);
+
+
             var li = document.createElement("li");
             li.setAttribute("class", "design");
+            li.setAttribute("id","pid"+temp++);
+
             var text = document.createTextNode(val.value);
             var lbt = lb.appendChild(text);
             li.appendChild(bt1);
@@ -27,6 +51,22 @@ function search(val) {
             ul[0].appendChild(li);
             val.value = "";
             arr.push(obj);
+
         }
     }
 }
+
+// //deleteIt function
+// function deleteIt(val){
+//    var el = document.getElementsByClassName("btn2");
+//    btn2[2].style.display = 'none';
+//    console.log(el[0]);
+// // console.log(val)
+// }
+
+
+// var ap = document.getElementsByClassName("append");
+// ap.addEventListener("click",function (a) {
+//     console.log(a.target);
+// },false);
+
